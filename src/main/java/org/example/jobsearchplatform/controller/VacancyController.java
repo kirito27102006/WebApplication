26fb03bab9bc1fd9749a365e9c64ec8858a9898c
Andrey <andrejignatuk372@gmail.com>
2026-02-24 15:23:13 +0300

    eight

--- src/main/java/org/example/jobsearchplatform/controller/VacancyController.java
+++ src/main/java/org/example/jobsearchplatform/controller/VacancyController.java
@@ -6,7 +6,6 @@ import org.example.jobsearchplatform.dto.VacancyResponse;
 import org.example.jobsearchplatform.service.VacancyService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
